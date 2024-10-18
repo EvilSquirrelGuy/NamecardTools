869e820ca530f5357ccfdc030494cb1aa762ecfe
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2024-10-18 20:33:34 +0100

    abstract footer link generation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faD, faDownload, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Url } from "next/dist/shared/lib/router/router";
 
 export default function Footer() {
+
+  const FooterLink = ({text, url, icon}: {text: string, url: string, icon: IconDefinition}) => (
+    <a className="text-violet-500 hover:text-violet-300 transition-all" href={url}>
+      <FontAwesomeIcon icon={icon} className="pr-2 fa-sm" />
+      {text}
+    </a>
+  )
+
   return (
     <footer className="flex flex-row gap-16 items-center sm:items-start w-full bottom-0 justify-center p-4 pb-12">
-      <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://github.com/EvilSquirrelGuy">
-        <FontAwesomeIcon icon={faGithub} className="pr-2 fa-sm"></FontAwesomeIcon>
-        Check out my GitHub
-      </a>
-      <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://genshin.hoyoverse.com/en">
-        <FontAwesomeIcon icon={faDownload} className="pr-2 fa-sm"></FontAwesomeIcon>
-        Genshin Impact
-      </a>
+      <FooterLink text="Project Github" icon={faGithub} url="https://github.com/EvilSquirrelGuy/NamecardTools" />
+      <FooterLink text="Download Genshin" icon={faDownload} url="https://genshin.hoyoverse.com/en/home" />
     </footer>
   )
 }
