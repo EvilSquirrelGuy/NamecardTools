aa9e527cf1b440b5cdd80828aa500b2f94827537
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2024-10-18 11:52:31 +0100

    add font awesome icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 // import Image from "next/image";
 import { roboto_slab } from "@/app/fonts";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 
 export default function Home() {
@@ -10,11 +16,20 @@ export default function Home() {
           Namecard Tools
         </h1>
         <p>🚧 Under construction...</p>
-        <a className="flex shadow-md hover:shadow-xl bg-violet-600 border-violet-800 hover:bg-violet-500 transition-all px-4 py-2 align-middle rounded-lg" href="https://github.com/EvilSquirrelGuy/NamecardTools">Follow Development ➡️</a>
+        <a className="flex shadow-md hover:shadow-xl bg-violet-600 border-violet-800 hover:bg-violet-500 transition-all px-4 py-2 items-center rounded-lg" href="https://github.com/EvilSquirrelGuy/NamecardTools">
+          Follow Development
+          <FontAwesomeIcon icon={faArrowRight} className="pl-2 fa-md"></FontAwesomeIcon>
+        </a>
       </main>
       <footer className="flex flex-row gap-8 row-start-3 items-center sm:items-start">
-        <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://github.com/EvilSquirrelGuy">Check out my GitHub!</a>
-        <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://genshin.hoyoverse.com/en">Genshin Impact</a>
+        <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://github.com/EvilSquirrelGuy">
+          <FontAwesomeIcon icon={faGithub} className="pr-2 fa-sm"></FontAwesomeIcon>
+          Check out my GitHub
+        </a>
+        <a className="text-violet-500 hover:text-violet-300 transition-all" href="https://genshin.hoyoverse.com/en">
+          <FontAwesomeIcon icon={faDownload} className="pr-2 fa-sm"></FontAwesomeIcon>
+          Genshin Impact
+        </a>
       </footer>
     </div>
   );
