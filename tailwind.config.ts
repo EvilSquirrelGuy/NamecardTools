031b50c763c63582eaa90d0911b0b355eb713278
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2024-10-21 20:44:22 +0100

    switch responsive to desktop-first

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,14 @@ const config: Config = {
     },
     fontFamily: {
       "display": ["Roboto", "Helvetica", "sans-serif"]
+    },
+    // override media queries to be desktop-first
+    screens: {
+      '2xl': {'max': '1535px'}, // => @media (max-width: 1535px) { ... }
+      'xl': {'max': '1279px'}, // => @media (max-width: 1279px) { ... }
+      'lg': {'max': '1023px'}, // => @media (max-width: 1023px) { ... }
+      'md': {'max': '767px'}, // => @media (max-width: 767px) { ... }
+      'sm': {'max': '639px'}, // => @media (max-width: 639px) { ... }
     }
   },
   plugins: [],
