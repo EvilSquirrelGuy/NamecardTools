753fb5ba575fa21ef31630010dc01ce31f495c7b
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2025-05-17 17:49:13 +0100

    clean slate (again)

--- packages/api/src/handlers/dummy.ts
+++
@@ -1,6 +0,0 @@
-import type { Context } from "hono"
-
-export function dummy(c: Context) {
-  c.status(500);
-  return c.json({ err: "Not Implemented" })
-}
--- packages/api/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono"
-
-const indexRoute = new Hono()
-
-indexRoute.get("/ping", (c) => c.text("Pong!"))
-indexRoute.get("/", (c) => c.json({ msg: "NamecardTools API is working!" }))
-
-export default indexRoute
